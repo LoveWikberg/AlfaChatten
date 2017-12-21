152dc52d2b3409fd1eb4c13d8c814965f78f5e66
Love <love.wick93@gmail.com>
2017-12-21 15:52:44 +0100

    Signalr up and running

---
+++ AlfaChatten/AlfaChatten/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlfaChatten.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Groups.AddAsync("connId", "gruppnamn");
+            List<string> caller = new List<string>
+            {
+                Context.ConnectionId
+            };
+            // Call the broadcastMessage method to update clients.
+            Clients.AllExcept(caller).InvokeAsync("broadcastMessage", name, message);
+            //Clients.Group("asd").InvokeAsync("broadcastMessage", name, message);
+        }
+    }
+}
